6ba2d829210b721739fb434d06a3e4680d6a08e0
TRG <trg01@trgaul.com>
2020-08-20 14:14:09 -0500

    Simple request to download data from json source

--- cocoapod01/ContentView.swift
+++ cocoapod01/ContentView.swift
@@ -6,16 +6,24 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+      .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+        let request = AF.request("https://swapi.dev/api/films")
+        // 2
+        request.responseJSON { (data) in
+          print(data)
+        }
+      })
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
