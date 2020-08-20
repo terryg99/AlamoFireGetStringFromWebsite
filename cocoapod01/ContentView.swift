d8849977b7d8294e0e6e19b5f6b18de2e611cafc
TRG <trg01@trgaul.com>
2020-08-20 07:11:24 -0500

    Initial Commit

---
+++ cocoapod01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cocoapod01
+//
+//  Created by Terry Gaul on 8/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
